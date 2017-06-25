96c5399030cf4fd3fda123047b6a67bcefc329b9
Rifat Akhmetov <rifat.akhmetov@gmail.com>
2017-06-25 14:12:32 +0300

    Create migrations for new Identity properties

---
+++ data/Migrations/Version20170625103740.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170625103740 extends AbstractMigration
+{
+    public function getDescription()
+    {
+        $description = 'This migration adds new fields to identity table';
+        return $description;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->getTable('identity');
+        $table->addColumn('range', 'integer', ['notnull'=>false]);
+        $table->addColumn('identity_id', 'string', ['notnull'=>true, 'length'=>50]);
+        $table->addColumn('description', 'text', ['notnull'=>false]);
+        $table->addColumn('date_of_issue', 'datetime', ['notnull'=>true]);
+        $table->addColumn('date_of_expire', 'datetime', ['notnull'=>true]);
+        $table->addColumn('authority', 'string', ['notnull'=>false, 'length'=>250]);
+        $table->addColumn('is_valid', 'boolean', ['notnull'=>true, 'default'=>1]);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $table = $schema->getTable('identity');
+        $table->dropColumn('range');
+        $table->dropColumn('identity_id');
+        $table->dropColumn('description');
+        $table->dropColumn('date_of_issue');
+        $table->dropColumn('date_of_expire');
+        $table->dropColumn('authority');
+        $table->dropColumn('is_valid');
+    }
+}
---
+++ data/Migrations/Version20170625110147.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170625110147 extends AbstractMigration
+{
+    public function getDescription()
+    {
+        $description = 'This migration adds index to identity table.';
+        return $description;
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $table = $schema->getTable('identity');
+        $table->addUniqueIndex(['ident_type', 'identity_id'], 'identity_type_id_index');
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $table = $schema->getTable('identity');
+        $table->dropIndex('identity_type_id_index');
+    }
+}
